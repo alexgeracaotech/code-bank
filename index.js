f78ffc98495c7b9f5d6a698ac3e145afd9fd2c65
Alex Bessa <alexbessa.dev@gmail.com>
2026-09-12 15:36:54 -0300

    feat: adicionar operacoes interativas por manipulacao de estrutura com armazenamento por sessao

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
+function greet() {
+  const username = window.prompt('Digite seu nome:');
+
+  if (!username) {
+    if (username !== '')
+      return window.alert('Operação cancelada.');
+
+    window.alert(`Olá! Seja bem vindo.`);
+    window.sessionStorage.setItem('username', username);
+    return window.location.href = './src/app.html';
+  }
+
+  window.alert(`Olá, ${username}! Seja bem vindo.`);
+
+  window.sessionStorage.setItem('username', username);
   window.location.href = './src/app.html';
-});
+}
+
+button.addEventListener('click', () => greet());
+
+
--- src/app.js
+++ src/app.js
@@ -1,141 +1,106 @@
-const logout = document.querySelector('#logout');
-const start = document.querySelector('#start');
+const exit = document.querySelector('#exit');
+const check = document.querySelector('#check');
+const deposit = document.querySelector('#deposit');
+const cashout = document.querySelector('#cashout');
+const statement = document.querySelector('#statement');
 
-logout.addEventListener('click', () => {
-  window.location.href = '../index.html';
-});
+let balance = 0;
+const statementList = [];
 
-start.addEventListener('click', () => {
-  let repeat = true;
+const operations = {
+  check: () => window.alert(`Saldo atual: R$ ${balance}`),
+  deposit: () => {
+    const response = window.prompt('Valor do depósito:');
 
-  function greet() {
-    const username = window.prompt('Digite seu nome:');
+    if (response === null) return window.alert('Operação cancelada.');
+
+    const value = Number(response);
+
+    if (value < 1) {
+      window.alert('Digite um valor válido.');
+      return operations.deposit();
+    } else if (isNaN(value)) {
+      window.alert('Digite um valor válido.');
+      return operations.deposit();
+    }
+
+    balance += value;
+
+    statementList[statementList.length] = {
+      type: 'Depósito',
+      value: value
+    };
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  cashout: () => {
+    const response = window.prompt('Valor do saque:');
+
+    if (response === null) return window.alert('Operação cancelada.');
+
+    const value = Number(response);
+
+    if (value < 1) {
+      window.alert('Digite um valor válido.');
+      return operations.cashout();
+    } else if (isNaN(value)) {
+      window.alert('Digite um valor válido.');
+      return operations.cashout();
+    } else if (value > balance) {
+      window.alert('Saldo indisponível.');
+      return operations.cashout();
+    }
+
+    balance -= value;
+
+    statementList[statementList.length] = {
+      type: 'Saque',
+      value: value
+    };
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  statement: () => {
+    if (statementList.length === 0) {
+      return window.alert('Nenhuma transação realizada.');
+    }
+
+    let text = 'Extrato:\n\n';
+
+    for (let i = 0; i < statementList.length; i++) {
+      text += `${statementList[i].type} = R$ ${statementList[i].value}\n`;
+    }
+
+    window.alert(text);
+  },
+  exit: () => {
+    const response = window.confirm('Deseja realmente sair?');
+
+    if (!response) return;
+
+    const username = sessionStorage.getItem('username');
 
     if (!username) {
-      if (username !== '') {
-        repeat = false;
-        return window.alert('Operação cancelada.');
-      }
-
-      return window.alert(`Olá! Seja bem vindo.`);
-    }
-
-    window.alert(`Olá, ${username}! Seja bem vindo.`);
-  }
-
-  greet();
-
-  let balance = 0;
-  const statement = [];
-
-  const operations = {
-    check: () => window.alert(`Saldo atual: R$ ${balance}`),
-    deposit: () => {
-      const response = window.prompt('Valor do depósito:');
-
-      if (response === null) return window.alert('Operação cancelada.');
-
-      const value = Number(response);
-
-      if (value < 1) {
-        window.alert('Digite um valor válido.');
-        return operations.deposit();
-      } else if (isNaN(value)) {
-        window.alert('Digite um valor válido.');
-        return operations.deposit();
-      }
-
-      balance += value;
-
-      statement[statement.length] = {
-        type: 'Depósito',
-        value: value
-      };
-
-      window.alert(`Saldo atual: R$ ${balance}`);
-    },
-    cashout: () => {
-      const response = window.prompt('Valor do saque:');
-
-      if (response === null) return window.alert('Operação cancelada.');
-
-      const value = Number(response);
-
-      if (value < 1) {
-        window.alert('Digite um valor válido.');
-        return operations.cashout();
-      } else if (isNaN(value)) {
-        window.alert('Digite um valor válido.');
-        return operations.cashout();
-      } else if (value > balance) {
-        window.alert('Saldo indisponível.');
-        return operations.cashout();
-      }
-
-      balance -= value;
-
-      statement[statement.length] = {
-        type: 'Saque',
-        value: value
-      };
-
-      window.alert(`Saldo atual: R$ ${balance}`);
-    },
-    statement: () => {
-      if (statement.length === 0) {
-        return window.alert('Nenhuma transação realizada.');
-      }
-
-      let text = 'Extrato:\n\n';
-
-      for (let i = 0; i < statement.length; i++) {
-        text += `${statement[i].type} = R$ ${statement[i].value}\n`;
-      }
-
-      window.alert(text);
-    },
-    exit: () => {
-      const response = window.confirm('Deseja realmente sair?');
-
-      if (!response) return;
-
-      window.alert('Foi um prazer atendê-lo.');
-      repeat = false;
-    },
-    invalid: () => window.alert('Número inválido.')
-  }
-
-  while (repeat) {
-    const operation = window.prompt(`
-    Escolha uma operação:
-  
-    1 - Consultar
-    2 - Depositar
-    3 - Sacar
-    4 - Extrato
-    5 - Sair
-  `);
-
-    if (operation === null) {
-      const response = window.confirm('Deseja realmente cancelar?');
-
-      if (!response) continue;
-
-      window.alert('Operaçao cancelada.');
-      repeat = false;
-    } else if (operation === '') {
-      window.alert('Digite um número válido.');
-    } else if (isNaN(operation)) {
-      window.alert('Digite um número válido.');
+      window.alert(`Foi um prazer atendê-lo.`);
     } else {
-      switch (Number(operation)) {
-        case 1: operations.check(); break;
-        case 2: operations.deposit(); break;
-        case 3: operations.cashout(); break;
-        case 4: operations.statement(); break;
-        case 5: operations.exit(); break;
-        default: operations.invalid();
-      }
+      window.alert(`Foi um prazer atendê-lo, ${username}.`);
     }
+
+    exit.disabled = true;
+
+    setTimeout(() => exit.textContent = 'Saindo.', 0);
+    setTimeout(() => exit.textContent = 'Saindo..', 500);
+    setTimeout(() => exit.textContent = 'Saindo...', 1000);
+    setTimeout(() => exit.textContent = 'Saindo.', 1500);
+    setTimeout(() => exit.textContent = 'Saindo..', 2000);
+    setTimeout(() => exit.textContent = 'Saindo...', 2500);
+
+    setTimeout(() => window.location.href = '../index.html', 3000);
   }
-});
+}
+
+exit.addEventListener('click', () => operations.exit());
+check.addEventListener('click', () => operations.check());
+deposit.addEventListener('click', () => operations.deposit());
+cashout.addEventListener('click', () => operations.cashout());
+statement.addEventListener('click', () => operations.statement());
